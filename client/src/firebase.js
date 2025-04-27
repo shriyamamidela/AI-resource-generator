abdae80e827ea629a02527636a58b4b23103c81d
shriyaaaa <shriyamamidela2004@gmail.com>
2025-04-27 11:17:39 +0530

    refactor: move Firebase credentials to environment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfzu9Om6m7NMhokwYBXsm0Y4hv7QWcEzg",
-  authDomain: "flashcardgen-9529f.firebaseapp.com",
-  projectId: "flashcardgen-9529f",
-  storageBucket: "flashcardgen-9529f.firebasestorage.app",
-  messagingSenderId: "257961503156",
-  appId: "1:257961503156:web:b0358d36e13cbf10082ae1",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
